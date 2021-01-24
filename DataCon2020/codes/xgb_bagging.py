7c1d65847c74d4e9d926499e921f6ac8717b987f
yhf <yhf5fhy@gmail.com>
2021-01-24 20:11:20 +0800

    Create xgb_bagging.py

---
+++ DataCon2020/codes/xgb_bagging.py
@@ -0,0 +1,16 @@
+from xgboost import XGBClassifier
+
+# 适用于训练数据较少，且预测值抖动现象明显的场合
+result = []
+for i in range(10):
+    train_data, test_data, train_label, test_label = train_test_split(train_tfidf_features, 
+                                                                      labels, 
+                                                                      test_size=0.2, 
+                                                                      random_state=i)
+
+    model = XGBClassifier(n_estimators=100) 
+    model.fit(train_data, train_label)
+    y_pred = model.predict(test_tfidf_features)
+    result.append(y_pred)
+y_pred = np.array(result).mean(axis=0)
+y_pred_end = [1 if i >= 0.5 else 0 for i in y_pred]
